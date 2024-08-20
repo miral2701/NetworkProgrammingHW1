4608b1eca63901d7437c2c882f855e7e779827e3
Mira Madani <miramadani2701@gmail.com>
2024-08-20 15:14:28 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectToServer();
+            Console.ReadLine();
+        }
+
+        static void ConnectToServer()
+
+        {
+
+            try
+
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 8888);
+                int choice = 1;
+                Console.WriteLine("Подключено к серверу...");
+                while (choice==1)
+                {
+                    Console.WriteLine("Введите название двух валют->");
+                    string message = Console.ReadLine();
+
+
+                    NetworkStream stream = client.GetStream();
+
+
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+
+                    stream.Write(data, 0, data.Length);
+
+
+                    byte[] data2 = new byte[2048];
+                    int size = stream.Read(data2, 0, data2.Length);
+                    string message2 = Encoding.UTF8.GetString(data2, 0, data2.Length);
+                    Console.WriteLine(message2);
+                    Console.Write("Continue?(1-yes 0-no)");
+                   choice=Convert.ToInt32(Console.ReadLine());
+
+                }
+                client.Close();
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine("Ошибка: " + ex.Message);
+
+            }
+
+        }
+    }
+}
---
+++ NetworkProgrammingHW1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace NetworkProgrammingHW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StartServer();
+        }
+
+        public static void StartServer()
+
+        {
+
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+
+            int port = 8888;
+
+            TcpListener listener = new TcpListener(ipAddress, port);
+
+            try
+
+            {
+
+                listener.Start();
+
+                Console.WriteLine("Сервер запущен...");
+
+
+               
+
+                    TcpClient client = listener.AcceptTcpClient();
+
+                    Console.WriteLine("Новый клиент подключен.");
+                    Console.WriteLine(DateTime.Now);
+
+                    NetworkStream stream = client.GetStream();
+                while (client.Connected)
+                {
+
+                    byte[] data = new byte[8];
+
+                    int size = stream.Read(data, 0, data.Length);
+                    StringBuilder message = new StringBuilder();
+                    message.Append(Encoding.UTF8.GetString(data));
+                    string res = new string("");
+                    if (message.ToString() == "USD EURO")
+                    {
+                        res = "Курс долара к евро-> 1:09";
+
+                    }
+                    else if (message.ToString() == "EURO USD")
+                    {
+                        res = "Курс евро к долару-> 1:1.11 ";
+
+                    }
+                    else
+                    {
+                        res = "Error";
+
+                    }
+                    byte[] response = Encoding.UTF8.GetBytes(res.ToString());
+                    stream.Write(response, 0, response.Length);
+                    client.Close();
+                }
+                
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine("Ошибка: " + ex.Message);
+
+            }
+
+            finally
+
+            {
+
+                listener.Stop();
+
+            }
+
+        }
+    }
+}
